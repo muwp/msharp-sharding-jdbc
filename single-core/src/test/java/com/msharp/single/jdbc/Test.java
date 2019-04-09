9086f159298cbaced45aaf69c8fb03bf2728e125
scmadmin <1059233029@qq.com>
2019-04-09 16:52:27 +0800

    modify

--- single-core/src/test/java/com/msharp/single/jdbc/Test.java
+++ single-core/src/test/java/com/msharp/single/jdbc/Test.java
@@ -1,10 +1,12 @@
 package com.msharp.single.jdbc;
 
 
+import com.msharp.single.jdbc.datasource.jdbc.DruidSingleDataSource;
 import com.msharp.single.jdbc.datasource.jdbc.SingleDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * Test
@@ -16,16 +18,19 @@ import java.sql.PreparedStatement;
 public class Test {
 
     public static void main(String[] args) throws Throwable {
-        final SingleDataSource singleDataSource = new SingleDataSource();
+        final DruidSingleDataSource singleDataSource = new DruidSingleDataSource();
         singleDataSource.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/sharding_jdbc_0?characterEncoding=UTF8&amp;socketTimeout=60000&amp;useSSL=false");
         singleDataSource.setUser("root");
         singleDataSource.setPassword("122816");
-        singleDataSource.setPoolType("druid");
         singleDataSource.init();
         final Connection connection = singleDataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("select * from product");
-        preparedStatement.executeQuery();
+        ResultSet resultSet= preparedStatement.executeQuery();
 
+        while (resultSet.next()){
+          String value=  resultSet.getString(1);
+          System.out.println(value);
+        }
         System.out.println(connection);
 
         Thread.sleep(399999);
